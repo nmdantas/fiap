441a179406b79c1845cb959a23c2300edd7d7163
Nicholas M. Dantas <n.moraes.dantas@gmail.com>
2018-09-19 22:11:17 -0300

    Update SharedContent.java

--- EducaLab/app/src/main/java/br/com/fiap/educalab/shared/SharedContent.java
+++ EducaLab/app/src/main/java/br/com/fiap/educalab/shared/SharedContent.java
@@ -32,6 +32,6 @@ public class SharedContent {
     }
     
     public static boolean expired() {
-        return expireDate != null && expireDate.isBefore(LocalDateTime.now);
+        return expireDate != null && expireDate.isBefore(LocalDateTime.now());
     }
 }

226d5a1c6041b8963b649c3a5a12b04f8da4759e
Nicholas M. Dantas <n.moraes.dantas@gmail.com>
2018-09-19 21:40:38 -0300

    Update SharedContent.java

--- EducaLab/app/src/main/java/br/com/fiap/educalab/shared/SharedContent.java
+++ EducaLab/app/src/main/java/br/com/fiap/educalab/shared/SharedContent.java
@@ -1,5 +1,6 @@
 package br.com.fiap.educalab.shared;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -7,6 +8,8 @@ import java.util.Set;
 
 public class SharedContent {
     private static final Set<String> PACKAGE_BLACK_LIST = new HashSet<>();
+    
+    private static LocalDate expireDate = null;
 
     public static void addToBlackList(String packageName) {
         PACKAGE_BLACK_LIST.add(packageName);
@@ -18,5 +21,13 @@ public class SharedContent {
 
     public static boolean shouldBlock(String packageName) {
         return PACKAGE_BLACK_LIST.contains(packageName);
+    }
+    
+    public static LocalDate getExpireDate() {
+        return expireDate;
+    }
+    
+    public static void setExpireDate(LocalDate date) {
+        expireDate = date;
     }
 }
